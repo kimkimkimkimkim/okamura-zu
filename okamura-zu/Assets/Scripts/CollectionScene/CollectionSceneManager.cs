96f7762a3992e8d3408b0edaca14b9ac1344b9cc
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-09-11 00:13:09 +0900

    機能追加　アイテム生成取得機能実装

---
+++ okamura-zu/Assets/Scripts/CollectionScene/CollectionSceneManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CollectionSceneManager : MonoBehaviour
+{   
+    public GameObject itemPrefab;
+    public GameObject canvasGame;
+    public int maxItemCount = 10; //アイテムの最大数
+    public float respawnTime = 5; //アイテムが発生する秒数
+
+    private int currentItemCount = 0; //現在のアイテム数
+    private DateTime lastDateTime; //前回アイテムを生成した時間
+
+    void Start(){
+        currentItemCount = 10;
+        //初期オーブ生成
+        for(int i=0;i<currentItemCount;i++){
+            CreateItem();
+        }
+
+        //初期設定
+        lastDateTime = DateTime.UtcNow;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(currentItemCount < maxItemCount){
+            TimeSpan timeSpan = DateTime.UtcNow - lastDateTime;
+
+            if(timeSpan >= TimeSpan.FromSeconds(respawnTime)){
+                while(timeSpan >= TimeSpan.FromSeconds(respawnTime)){
+                    CreateNewItem();
+                    timeSpan -= TimeSpan.FromSeconds(respawnTime);
+                }
+            }
+        }
+    }
+
+    //新しいアイテムの生成
+    public void CreateNewItem(){
+        lastDateTime = DateTime.UtcNow;
+        if(currentItemCount >= maxItemCount) return;
+
+        CreateItem();
+        currentItemCount++;
+    }
+
+    //アイテム生成
+    public void CreateItem(){
+        GameObject item = (GameObject)Instantiate(itemPrefab);
+        item.transform.SetParent(canvasGame.transform, false);
+        item.transform.localPosition = new Vector3(
+            UnityEngine.Random.Range(-300.0f, 300.0f),
+            UnityEngine.Random.Range(-140.0f, -500.0f),
+            0f);
+    }
+
+    //アイテム入手
+    public void GetItem(){
+        currentItemCount--;
+    }
+}
---
+++ okamura-zu/Assets/Scripts/CollectionScene/ItemManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{   
+    private GameObject collectionScreenManager;
+
+    void Start(){
+        collectionScreenManager = GameObject.Find("CollectionScreenManager");
+    }
+
+    public void TouchItem(){
+        if(Input.GetMouseButton(0) == false) return;
+
+        collectionScreenManager.GetComponent<CollectionSceneManager>().GetItem();
+        Destroy(this.gameObject);
+    }
+}
