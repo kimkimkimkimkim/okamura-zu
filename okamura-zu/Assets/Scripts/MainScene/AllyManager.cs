4518cb7e88af3892a7a502864ddcad2ac2646b60
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-11-10 20:01:17 +0900

    機能追加　仲間の自動攻撃追加

---
+++ okamura-zu/Assets/Scripts/MainScene/AllyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyManager : MonoBehaviour
+{
+    //オブジェクト参照
+
+    //変数宣言
+    private GameObject mainSceneManager;
+    private int attackPower = 1;
+    //private float attackRate = 1f; //一秒間に何回攻撃するか
+    private float attackInterval = 1f; //攻撃と攻撃の間隔[s]
+    private float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainSceneManager = GameObject.Find("MainSceneManager");
+    }
+
+    void FixedUpdate(){
+        time += Time.deltaTime;
+        if(time>=attackInterval){
+            //攻撃
+            mainSceneManager.GetComponent<MainSceneManager>().Attack(attackPower);
+            time = 0;
+        }
+    }
+
+}
