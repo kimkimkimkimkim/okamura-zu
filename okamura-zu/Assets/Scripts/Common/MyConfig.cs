f074bc4feeea8c780fc12a81e60285d19892e2f4
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2019-12-15 01:31:58 +0900

    機能追加　フッターメニュー作成

--- okamura-zu/Assets/Scripts/Common/MyConfig.cs
+++ okamura-zu/Assets/Scripts/Common/MyConfig.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public static class MyConfig
 {
-    public static float safeAreaTop = 88;
-    public static float safeAreaBottom = 68;
+    public static float safeAreaTop = 100;
+    public static float safeAreaBottom = 80;
     public static float headerHeight = 120;
-    public static float footerHeight = 200;
+    public static float footerHeight = 100;
     public static float screenWidth = 1125;
     public static float screenHeight = 2436;
 }
--- okamura-zu/Assets/Scripts/HeaderFooter/HeaderFooterManager.cs
+++ okamura-zu/Assets/Scripts/HeaderFooter/HeaderFooterManager.cs
@@ -11,29 +11,68 @@ public class HeaderFooterManager : MonoBehaviour
     //オブジェクト参照
     public List<GameObject> footerButtons = new List<GameObject>();
     public GameObject navigationManager;
+    public GameObject footerMenu;
+
+    //変数宣言
+    private float menuHeight = 727.7f;
+    private GameObject isOpenMenuButton = null;
 
     // Start is called before the first frame update
     void Start()
     {
         InitialSetObserver();
+        InitialSetActice();
     }
 
     void InitialSetObserver(){
         footerButtons[0].AddComponent<ObservableEventTrigger>()
             .OnPointerClickAsObservable()
-            .Subscribe(_ => navigationManager.GetComponent<NavigationManager>().ToMoveIndex(-1));
+            .Subscribe(_ => OnPressFooterButton(footerButtons[0]));
         footerButtons[1].AddComponent<ObservableEventTrigger>()
             .OnPointerClickAsObservable()
-            .Subscribe(_ => navigationManager.GetComponent<NavigationManager>().ToMoveIndex(0));
+            .Subscribe(_ => OnPressFooterButton(footerButtons[1]));
         footerButtons[2].AddComponent<ObservableEventTrigger>()
             .OnPointerClickAsObservable()
-            .Subscribe(_ => navigationManager.GetComponent<NavigationManager>().ToMoveIndex(1));
+            .Subscribe(_ => OnPressFooterButton(footerButtons[2]));
+    }
+
+    void InitialSetActice(){
+        footerMenu.SetActive(false);
     }
 
     void Move(int targetIndex){
         int index = navigationManager.GetComponent<NavigationManager>().index;
         if(index == targetIndex)return;
 
+    }
+
+    void OnPressFooterButton(GameObject footerBtn){
+        if(isOpenMenuButton == footerBtn){
+            //今開いてるメニューが押したフッターのメニューだったら閉じる
+            CloseMenu();
+            isOpenMenuButton = null;
+        }else{
+            //何も開いてないときは開く
+            isOpenMenuButton = footerBtn;
+            OpenMenu();
+        }
+    }
+
+    void OpenMenu(){
+        footerMenu.transform.localPosition = new Vector3(0,-menuHeight,0);
+        footerMenu.SetActive(true);
+
+        footerMenu.transform.DOLocalMove(
+            new Vector3(0,100,0),
+            0.3f
+        );
+    }
+
+    void CloseMenu(){
+        footerMenu.transform.DOLocalMove(
+            new Vector3(0,-menuHeight,0),
+            0.3f
+        ).OnComplete(() => footerMenu.SetActive(false));
     }
 
     // Update is called once per frame
